b8b15bef252b3400536b7eab8e26bff6bbb04f2e
Jack <jtursunboev@bk.ru>
2021-11-03 13:17:53 +0900

    for_operatori yangilanidi

--- for_operatiori.py
+++ for_operatiori.py
@@ -5,21 +5,21 @@ Created on Mon Oct 25 20:00:52 2021
 @author: User
 """
 
- #dostlar = ['rustam','zohit','behzod']
-#for dost in dostlar:
-    #print(f"hurmatli dositim  {dost.title()} sizni toyimga taklif etaman")
-   # print('hurmat bilan Javohir')
+dostlar = ['rustam','zohit','behzod']
+for dost in dostlar:
+    print(f"hurmatli dositim  {dost.title()} sizni toyimga taklif etaman")
+    print('hurmat bilan Javohir')
    
-#sonlar = list(range(1, 11))
-#for son in sonlar:
- #      print(f"{son} ning kvadirati {son**2} ga teng")
+sonlar = list(range(1, 11))
+for son in sonlar:
+       print(f"{son} ning kvadirati {son**2} ga teng")
  
-#sonlar = list(range(11))
-#kv = []
-#for son in sonlar:
- #   kv.append(son**2)
-#    print(sonlar)
-#    print(kv)
+sonlar = list(range(11))
+kv = []
+for son in sonlar:
+    kv.append(son**2)
+    print(sonlar)
+    print(kv)
 
 x = []
 print('5 ta dostingizni kiriting???')
